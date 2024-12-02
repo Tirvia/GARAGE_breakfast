9dd4192b953b04c125278053a6c2779f78f4cf09
Hanna <112614483+Tirvia@users.noreply.github.com>
2024-12-02 15:48:44 +0500

    завтраки возврат

--- js/jquery-1.11.0.js
+++ js/jquery-1.11.0.js
@@ -64,49 +64,7 @@ $( document ).ready(function() {
         {id:15,
                 question:'././images/Блин с твор соус и икрой1.png',
                 answer:'././images/Блин с твор соус и икрой.png'},
-        {id:16,
-         question:'././images/Драники верещака1.png',
-         answer:'././images/Драники верещака.png'},
-
-        {id:17,
-         question:'././images/драники с беки сыр1.png',
-         answer:'././images/драники с беки сыр.png'},
-
-        {id:18,
-                question:'././images/драники с дом колб1.png',
-                answer:'././images/драники с дом колб.png'},
-        {id:19,
-         question:'././images/драники с копч лососем1.png',
-         answer:'././images/драники с копч лососем.png'}, 
-
-        {id:20,
-         question:'././images/драники с мачанкой1.png',
-         answer:'././images/драники с мачанкой.png'},
-
-        {id:21,
-                question:'././images/драники с цыпленком и грибным соусом1.png',
-                answer:'././images/драники с цыпленком и грибным соусом.png'},
-        {id:22,
-         question:'././images/драники со слоб сол лос1.png',
-         answer:'././images/драники со слоб сол лос.png'},
-
-         {id:23,
-         question:'././images/драники со сметаной1.png',
-         answer:'././images/драники со сметаной.png'},
-
-        {id:24,
-                question:'././images/запеченые блины1.png',
-                answer:'././images/запеченые блины.png'},
-        {id:25,
-         question:'././images/налистники с вареньемь1.png',
-         answer:'././images/налистники с вареньем.png'},
-         {id:26,
-         question:'././images/налистники с мочанкой1.png',
-         answer:'././images/налистники с мочанкой.png'},
-         {id:27,
-                question:'././images/рулька с кортоф и яйцом1.png',
-                answer:'././images/рулька с кортоф и яйцом.png'}
-        ];
+                ];
         /*for (var i=0;i<cards.length;i++) {
         var randomcard = Math.floor(Math.random() * cards.id);
         console.log(randomcard);
@@ -115,7 +73,7 @@ $( document ).ready(function() {
         
         
         var ran;
-        ran= Math.round(Math.random()*26);
+        ran= Math.round(Math.random()*14);
         /*var blindcard=cards[20].question;
             var el=document.getElementById('card');
             //el[0].classList.add('img-responsive');//+=' flashtext';
@@ -130,7 +88,7 @@ $( document ).ready(function() {
         
         $('#question').on('click',function(){
         
-            ran= Math.round(Math.random()*26);
+            ran= Math.round(Math.random()*14);
         
             //console.log(ran);
             
